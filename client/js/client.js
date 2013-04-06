5ce8c7d9b3dbe5de2e05d7ab49b7b7779146b114
Elie Javice <ejavice@me.com>
2013-04-06 18:53:55 -0400

    start of create event

--- client/js/client.js
+++ client/js/client.js
@@ -37,5 +37,44 @@ if (Meteor.isClient) {
 			return false;
 		}
 	};
+	Template.content.about = function(){
+		if (Session.equals("page","about")){
+			return true;
+		}else{
+			return false;
+		}
+	};
 
+	Template.home.events({
+		'click li': function (event){
+			var li_id = event.currentTarget.className;
+			Session.set("page",""+li_id);
+		}
+	});
+
+	Template.create.events({
+		'click img': function (event){
+			var img_id = event.currentTarget.className;
+			if(img_id=="back"){
+				Session.set("page","home");
+			}else if(img_id=="submit"){
+				var eventname = document.getElementById("eventname").value.trim();
+				var eventlocation = document.getElementById("eventlocation").value.trim();
+				var eventspeaker = document.getElementById("eventspeaker").value.trim();
+				var eventemail = document.getElementById("eventemail").value.trim();
+				if(validateevent(eventname, eventemail)){
+					createEvent(eventname, eventlocation, eventspeaker, eventemail);
+				}else{
+					console.log("Invalid Event");
+				}
+			}
+		}
+	});
+}
+function validateevent(eventname, eventemail){
+	return true;
+}
+
+function createEvent(eventname, eventlocation, eventspeaker, eventemail){
+	Events.insert({name: eventname, location: eventlocation, speaker: eventspeaker, email: eventemail});
 }
